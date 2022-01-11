3691b1fd56df52dc66459d8ad0278d704e4afb0f
Anouar TOUMI <anouar.toumi@etu.univ-amu.fr>
2022-01-11 19:23:04 +0100

    adn.py fonction codé

--- adn.py
+++ adn.py
@@ -9,10 +9,22 @@ __author__ = 'Anouar TOUMI'
 
 
 
-def is_valid(adn_str):
-    pass
+def is_valid(adn_str: str)->bool:
+    not_nucleotide = 0
+    for nuc in adn_str:
+        if nuc not in ("ATCGatcg"):
+            not_nucleotide += 1
+        else: 
+            pass
+    if not_nucleotide == 0:
+        return True
+    else:
+        return False
 
 
-def get_valid_adn(prompt='chaîne : '):
-    pass
-
+def get_valid_adn(prompt="Chaine d'ADN : "):
+    input_sequence = input(prompt)
+    if is_valid(input_sequence) is True:
+        print("La chaine d'ADN est valide")
+    else:
+        print("La chaine d'ADN n'est pas valide")
