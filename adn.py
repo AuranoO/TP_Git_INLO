fb37a81b849c00c6230db8a2627b24eb3acbf9e0
Anouar TOUMI <anouar.toumi@etu.univ-amu.fr>
2022-01-17 10:43:04 +0100

    update adn.py

--- adn.py
+++ adn.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
-    get_valid_adn allows user to check if string is only nucleotide
-"""
-#Version develop
+# Version develop
 __author__ = 'Anouar TOUMI'
 
 
+def purify(adn_str: str)->str:
+    """
+    efe
+    """
 
-def is_valid(adn_str: str)->bool:
-    not_nucleotide = 0
-    for nuc in adn_str:
-        if nuc not in ("ATCGatcg"):
-            not_nucleotide += 1
-        else: 
-            pass
-    if not_nucleotide == 0:
-        return True
-    else:
+    return adn_str
+
+
+def is_valid(adn_str: str) -> bool:
+    """
+    efg
+    """
+    if adn_str is None or len(adn_str) == 0:
         return False
+    for nuc in adn_str:
+        if nuc not in "ATCGatcg":
+            return False
+        else:
+            return True
 
 
-def get_valid_adn(prompt="Chaine d'ADN : ")->str:
+def get_valid_adn(prompt="Donner votre chaine d'ADN : "):
+    """
+    efeg
+    """
     input_sequence = input(prompt)
-    if is_valid(input_sequence) is True:
-        print("La chaine d'ADN est valide")
-    else:
+    if is_valid(input_sequence) is not True:
         print("La chaine d'ADN n'est pas valide")
-    return input_sequence
+    else:
+        print("La chaine d'ADN est valide" + "\n" + input_sequence)
